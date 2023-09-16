b00de247b79ea6749d31beb3fa35d558297efa2e
DanielV100 <info@d-vollmer.de>
2023-09-16 11:53:28 +0200

    init

---
+++ src/components/login/Login.vue
@@ -0,0 +1,123 @@
+<script setup>
+import { ref } from 'vue'; 
+import * as Config from './config.js'; 
+const usernameInputBackground = ref(); 
+
+function validateUsername(event) {
+    let input = event.target.value; 
+    //if the input is empty default should be shown
+    if(input.length === 0) {
+        usernameInputBackground.value = ""; 
+    } else {
+        let lastInputChar = input[input.length-1]; 
+        if(Config.VALIDUSERNAMEINPUT.includes(lastInputChar)) {
+            usernameInputBackground.value = Config.BG_VALID; 
+        } else {
+            usernameInputBackground.value = Config.BG_INVALID; 
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <form action="">
+            <h1>{{Config.HEADING}}</h1>
+            <div class="input-box">
+                <input @input="validateUsername" :style="usernameInputBackground" type="text" placeholder="Username" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <button type="submit" class="btn">Login</button>
+        </form>
+    </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,400;6..12,800&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Nunito Sans", sans-serif;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: url('assets/background.jpg') no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+.wrapper {
+  width: 620px;
+  background: transparent;
+  border: 2px solid rgba(255, 255, 255, .2);
+  backdrop-filter: blur(20px);
+  box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+  color: #fff;
+  border-radius: 10px;
+  padding: 30px 40px;
+}
+
+.wrapper h1 {
+  font-size: 36px;
+  text-align: center;
+  font-weight: 800;
+}
+
+.wrapper .input-box {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  margin: 30px 0;
+}
+
+.input-box input {
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  border: none;
+  outline: none;
+  border: 2px solid rgba(255, 255, 255, .2);
+  border-radius: 40px;
+  font-size: 16px;
+  color: #fff;
+  padding: 20px 45px 20px 20px;
+}
+
+.input-box input::placeholder {
+  color: #fff;
+}
+
+.input-box i {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 20px;
+}
+
+
+.wrapper .btn {
+  width: 100%;
+  height: 45px;
+  background: #ffffff;
+  border: none;
+  outline: none;
+  border-radius: 40px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+  cursor: pointer;
+  font-size: 16px;
+  color: #333;
+  font-weight: 600;
+}
+
+</style>
---
+++ src/components/login/config.js
@@ -0,0 +1,6 @@
+export const HEADING = "Let's get Orderly, shall we? 🌱"; 
+
+/**** Username input ****/
+export const VALIDUSERNAMEINPUT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ."; 
+export const BG_VALID = "background: rgba(100, 200, 0, 0.3)"; 
+export const BG_INVALID = "background: rgba(240, 0, 20, 0.3)"; 
